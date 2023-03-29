f38fea6b754eac1859be6ee143b3692d2f9a2b62
Nikos Fliataris <75726267+nikosfliataris@users.noreply.github.com>
2023-03-29 20:05:14 +0300

    Update firebase.js

--- Front-end/src/firebase.js
+++ Front-end/src/firebase.js
@@ -1,13 +1,6 @@
 import firebase from "firebase";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyBKOH3_ytpmXcvOigJvRFELOZ_o-d37MUE",
-  authDomain: "fliataris-e-shop.firebaseapp.com",
-  projectId: "fliataris-e-shop",
-  storageBucket: "fliataris-e-shop.appspot.com",
-  messagingSenderId: "842299059679",
-  appId: "1:842299059679:web:bfa9467c38b15fa1fb286a",
-};
+const firebaseConfig = {""};
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth();
 const provider = new firebase.auth.GoogleAuthProvider();
